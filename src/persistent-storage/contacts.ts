82852d1a29cc0a5179aa71e53436cb5d2dee32ef
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-08-08 15:29:08 +0200

    Import wallet metadata from desktop wallet
    Closes #146

--- src/persistent-storage/contacts.ts
+++ src/persistent-storage/contacts.ts
@@ -98,3 +98,9 @@ export const deleteContact = async (contactId: Contact['id']) => {
 
   store.dispatch(contactDeletedFromPersistentStorage(contactId))
 }
+
+export const importContacts = async (contacts: ContactFormData[]) => {
+  for (const contact of contacts) {
+    await persistContact(contact)
+  }
+}
